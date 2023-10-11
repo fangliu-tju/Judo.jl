1baabe6757055c180bf1ad26f93814d1c2797c65
Documenter.jl <documenter@juliadocs.github.io>
2023-10-11 06:28:32 +0000

    build based on 154b638

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Judo","category":"page"},{"location":"#Judo","page":"Home","title":"Judo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Judo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Judo]","category":"page"}]
+}
